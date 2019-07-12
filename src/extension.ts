cdf3ae381aec47fd11e292df4db491fe72ad6f91
jeremybosscha <jeremybosscha@users.noreply.github.com>
2019-07-12 12:50:46 +0200

    Schoenen

--- src/extension.ts
+++ src/extension.ts
@@ -13,15 +13,58 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.caress-generator', () => {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
+		// const quickpicks = [new vscode.QuickInputButtons()]
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = getQuickPicks();
+		quickPick.show();
+
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+function getQuickPicks(): Array<vscode.QuickPickItem> {
+	let list = [];
+	const pageItem: vscode.QuickPickItem = {
+		'description': 'Page',
+		'detail': 'src/pages/',
+		'label': 'Generate a page'
+	};
+	const serviceItem: vscode.QuickPickItem = {
+		'description': 'Service',
+		'detail': 'src/services/',
+		'label': 'Generate a service'
+	};
+	const moduleItem: vscode.QuickPickItem = {
+		'description': 'Module',
+		'detail': 'src/modules/',
+		'label': 'Generate a module'
+	};
+	const sharedModuleItem: vscode.QuickPickItem = {
+		'description': 'Shared module',
+		'detail': 'src/sharedModules',
+		'label': 'Generate a shared module'
+	};
+
+	const entityItem: vscode.QuickPickItem = {
+		'description': 'Entity',
+		'detail': 'src/entities',
+		'label': 'Generate an entity'
+	};
+
+	list.push(pageItem);
+	list.push(serviceItem);
+	list.push(moduleItem);
+	list.push(sharedModuleItem);
+	list.push(entityItem);
+
+	return list;
+}
+
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
