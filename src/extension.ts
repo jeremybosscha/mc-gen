0181eeefcf8aa04887a648ccd8a16b14a876ea76
jeremybosscha <jeremybosscha@users.noreply.github.com>
2019-07-12 13:46:03 +0200

    cdssc
    dscds

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { addListener } from 'cluster';
+import { SpawnSyncOptionsWithStringEncoding } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,13 +16,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.caress-generator', () => {
-
+		let selectedType: GeneratorType;
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
 		// const quickpicks = [new vscode.QuickInputButtons()]
 		const quickPick = vscode.window.createQuickPick();
 		quickPick.items = getQuickPicks();
 		quickPick.show();
+		quickPick.onDidChangeSelection((item) => {
+			selectedType = JSON.parse(JSON.stringify(item));
+			vscode.window.showInformationMessage(JSON.stringify(item));
+			vscode.window.showInputBox({ placeHolder: 'Give the new ' + selectedType.description.toLowerCase() + ' a name...' }).then((x) => {
+				console.log(x);
+			});
+		});
+
 
 
 	});
@@ -28,30 +37,30 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function getQuickPicks(): Array<vscode.QuickPickItem> {
+function getQuickPicks(): Array<GeneratorType> {
 	let list = [];
-	const pageItem: vscode.QuickPickItem = {
+	const pageItem: GeneratorType = {
 		'description': 'Page',
 		'detail': 'src/pages/',
 		'label': 'Generate a page'
 	};
-	const serviceItem: vscode.QuickPickItem = {
+	const serviceItem: GeneratorType = {
 		'description': 'Service',
 		'detail': 'src/services/',
 		'label': 'Generate a service'
 	};
-	const moduleItem: vscode.QuickPickItem = {
+	const moduleItem: GeneratorType = {
 		'description': 'Module',
 		'detail': 'src/modules/',
 		'label': 'Generate a module'
 	};
-	const sharedModuleItem: vscode.QuickPickItem = {
+	const sharedModuleItem: GeneratorType = {
 		'description': 'Shared module',
 		'detail': 'src/sharedModules',
 		'label': 'Generate a shared module'
 	};
 
-	const entityItem: vscode.QuickPickItem = {
+	const entityItem: GeneratorType = {
 		'description': 'Entity',
 		'detail': 'src/entities',
 		'label': 'Generate an entity'
@@ -64,6 +73,18 @@ function getQuickPicks(): Array<vscode.QuickPickItem> {
 	list.push(entityItem);
 
 	return list;
+}
+
+class GeneratorType implements vscode.QuickPickItem {
+	label: string;
+	description: string;
+	detail: string;
+	constructor(label: string, description: string, detail: string) {
+		this.label = label;
+		this.description = description;
+		this.detail = detail;
+	}
+
 }
 
 // this method is called when your extension is deactivated
